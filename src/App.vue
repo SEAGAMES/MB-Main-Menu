9e5447ef336ac149574b411e3a15a6bc075f6446
DevGame <t.seagames@hotmail.com>
2024-03-07 10:49:11 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-toolbar color="blue-grey-lighten-4" dark>
+      <div><a href="https://mb.mahidol.ac.th/app/#/"></a></div>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Vuetify</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-export</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </div>
+  <router-view />
+</template>
---
+++ src/views/mainMenu.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-row align="center" justify="center" class="mt-3">
+    <!-- Loop through the array and create v-col for each element -->
+    <v-col cols="auto">
+      <v-card
+        class="mx-auto"
+        width="200"
+        a
+        href="https://mb.mahidol.ac.th/mb_certificate/#/"
+      >
+        <v-img
+          src="../assets/menu-img/certificate_menu.png"
+          width="140"
+          alt="Icon"
+          class="mx-auto"
+        ></v-img>
+        <v-card-actions class="d-flex justify-content-center">
+          <v-btn color="orange"> Certificate </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+
+    <v-col cols="auto">
+      <v-card
+        class="mx-auto"
+        width="200"
+        a
+        href="http://10.62.123.151:3200/mb_lab/#/mb-lab"
+      >
+        <v-img
+          src="../assets/menu-img/lab_menu.png"
+          width="140"
+          alt="Icon"
+          class="mx-auto"
+        ></v-img>
+        <v-card-actions class="d-flex justify-content-center">
+          <v-btn color="orange">Request Lab </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+</script>
